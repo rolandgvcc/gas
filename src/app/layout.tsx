a6d1d94c40acbec783864f84859f549ed560b527
Roland Adrian Gavrilescu <roland@x.ai>
2025-03-30 20:54:40 -0700

    init

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Next.js AI Lite App",
+  description: "AI with Next and AI SDK",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main className="bg-muted/50 flex h-100vh flex-1 flex-col">
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '@/components/ui/button'
+import { IconSeparator, IconVercel } from '@/components/ui/icons'
+import EnvCard from './cards/envcard'
+import { ThemeToggle } from '@/components/theme-toggle'
+
+export async function Header() {
+  return (
+    <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-background">  
+      <EnvCard />
+      <Link href="/" rel="nofollow" className="mr-2 font-bold">
+        Grok App Studio
+      </Link>
+      <ThemeToggle className="mr-2" />
+    </header>
+  )
+}
